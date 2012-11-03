1265f69292edf351ef3e7d9fe93f3944fe2901b4
Dillon Buchanan <thedillonb@gmail.com>
2012-11-03 17:40:28 -0400

    Added The objectDictionaryConverter

---
+++ GitHubSharp/Utils/ObjectToDictionaryConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace GitHubSharp.Utils
+{
+    public static class ObjectToDictionaryConverter
+    {
+        public static Dictionary<string, string> Convert<T>(T obj)
+        {
+            var dictionary = new Dictionary<string, string>();
+            var properties = obj.GetType().GetProperties();
+            foreach (var propertyInfo in properties)
+            {
+                var value = propertyInfo.GetValue(obj, null);
+                if (value != null)
+                {
+                    dictionary.Add(propertyInfo.Name.ToLower(), value.ToString());
+                }
+            }
+            return dictionary;
+        }
+    }
+}

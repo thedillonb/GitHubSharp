f403012ea01892182820a825f6b707f565ab6ea7
Dillon Buchanan <thedillonb@gmail.com>
2015-08-26 21:07:07 -0400

    Altered Directory Sturcture

---
+++ GitHubSharp/Controllers/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubSharp.Controllers
+{
+    public abstract class Controller
+    {
+        protected readonly Client Client;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="client">The GitHubSharp client object</param>
+        protected Controller(Client client)
+        {
+            Client = client;
+        }
+
+        /// <summary>
+        /// A URI representing this controller 
+        /// </summary>
+        public abstract String Uri { get; }
+    }
+}

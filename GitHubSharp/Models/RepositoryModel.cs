cdc50e326a5e98964ad7b0a3e92815add3b34b41
Dillon Buchanan <thedillonb@gmail.com>
2016-03-20 13:19:10 -0400

    Add DownloadUrl to content model

--- GitHubSharp/Models/RepositoryModel.cs
+++ GitHubSharp/Models/RepositoryModel.cs
@@ -107,7 +107,8 @@ namespace GitHubSharp.Models
         public string Sha { get; set; }
         public string Path { get; set; }
         public string GitUrl { get; set; }
-        public string HtmlUrl { get; set; }
+		public string HtmlUrl { get; set; }
+        public string DownloadUrl { get; set; }
         public string Encoding { get; set; }
         public string Url { get; set; }
         public long? Size { get; set; }
